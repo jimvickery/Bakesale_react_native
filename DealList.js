ed7783201f805e171bbc0f8ec8c8f8f80de5cb18
Jim Vickery <jimrv8@yahoo.com>
2018-08-08 00:58:14 -0500

    added proptypes. DId NOT install prpo-types b/c it breaks the app.

--- DealList.js
+++ DealList.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PropTypes from 'prop-types';
 
 
 class DealList extends React.Component {
+  static propTypes = {
+    deals: PropTypes.array.isRequired,
+  };
   render(){
     return(
       <View>
@@ -11,5 +15,14 @@ class DealList extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  list: {
+    backgroundColor:  '#eee',
+    flex: 1,
+    width: '100%',
+    paddingTop: 50,
+  },
+});
 
 export default DealList;
