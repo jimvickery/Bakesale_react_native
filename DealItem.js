d49f54b97ece574714266dee387463fcb4083b29
Jim Vickery <jimrv8@yahoo.com>
2018-08-09 19:28:09 -0500

    added styling to items

--- DealItem.js
+++ DealItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, Text, Image, View } from 'react-native';
-import priceDisplay from './util'
+import {priceDisplay} from './util';
 
 
 class DealItem extends React.Component{
@@ -12,25 +12,52 @@ class DealItem extends React.Component{
     const { deal } = this.props;
     return (
       <View>
-        <Image source={{ uri: this.props.deal.media[0] }} 
-          style={styles.image}
-        />
-        <View>
-          <Text>{deal.title} </Text>
-          <Text>{priceDisplay(deal.price)} </Text>
-          <Text>{deal.cause.name} {"\n"} </Text>
+        <Image source={{ uri: this.props.deal.media[0] }}  style={styles.image} />
+        <View style={styles.info}>
+          <Text style={styles.title}>{deal.title} </Text>
+          <View style={styles.footer}>
+            <Text style={styles.cause}>{deal.cause.name}</Text>
+            <Text style={styles.price}>{priceDisplay(deal.price)}</Text>
+          </View>
         </View>
-
       </View>
   );
   }
 }
 
 const styles = StyleSheet.create({
+  deal: {
+    marginHorizontal: 12,
+    marginTop: 12,
+  },
   image: {
     width: '100%',
     height: 150,
-  }
-})
+    backgroundColor: '#ccc',
+  },
+  info: {
+    padding: 10,
+    backgroundColor: '#fff',
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderTopWidth: 0,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  footer: {
+    flexDirection: 'row',
+  },
+  cause: {
+    flex: 2,
+  },
+  price: {
+    flex: 1,
+    textAlign: 'right',
+  },
+});
+
 
 export default DealItem;
--- util.js
+++ util.js
@@ -1,3 +1,3 @@
 export const priceDisplay = (priceInCents) => {
-  return `$${priceInCents/ 100}`;
+  return `$${priceInCents / 100}`;
 };
